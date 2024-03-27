507dd6934ca32fd92f817ab2700044b8cb0f80e7
Mr. Otabek <meliqoziyevotabek66@gmail.com>
2024-03-27 12:16:37 +0500

    select

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ document.getElementById('round1').innerHTML = Math.floor(Math.random() * 100);
 document.getElementById('round2').innerHTML = Math.floor(Math.random() * 100);
 
 var index = 0;
+var selectedOperation = '+';
 
 function refresh() {
     document.getElementById('round1').innerHTML = Math.floor(Math.random() * 100);
@@ -16,13 +17,35 @@ function refresh() {
 function check(){
     var round1 = parseInt(document.getElementById('round1').innerHTML);
     var round2 = parseInt(document.getElementById('round2').innerHTML);
-    var userGuess = parseInt(document.querySelector('input').value);
+    var userGuess = parseInt(document.getElementById('userAnswer').value);
     
-    var sum = round1 + round2;
+    var result;
+    switch(selectedOperation) {
+        case '+':
+            result = round1 + round2;
+            break;
+        case '-':
+            result = round1 - round2;
+            break;
+        case '*':
+            result = round1 * round2;
+            break;
+        case '/':
+            result = round1 / round2;
+            break;
+        case '%':
+            result = round1 % round2;
+            break;
+    }
     
-    if(userGuess === sum) {
+    if(userGuess === result) {
         alert("Congratulations!");
     } else {
-        alert("Incorrect! The correct answer is: " + sum);
+        alert("Incorrect! The correct answer is: " + result);
     }
 }
+
+document.getElementById('operation').addEventListener('change', function() {
+    selectedOperation = this.value;
+    refresh();
+});
